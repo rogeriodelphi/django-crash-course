34de1e44b8b94ce9ba4072dc6a09217cb2fc145e
rogerio.martins <rogerio.martins@enap.gov.br>
2021-06-19 00:49:07 -0300

    Create Todo Create View

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TodoForm
 from .models import Todo
 
 def todo_list(request):
@@ -16,3 +17,18 @@ def todo_detail(request, id):
         'todo': todo
     }
     return render(request, 'todo/todo_detail.html', context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+        # name = form.cleaned_data['name']
+        # due_date = form.cleaned_data['due_date']
+        # print(name, due_date)
+        #
+        # new_todo = Todo.objects.create(name=name, due_date=due_date)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/todo_create.html', context)
